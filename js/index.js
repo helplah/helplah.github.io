88be7e554827fe036cca1f89d92170c3ff515908
Jen <30688276+helplah@users.noreply.github.com>
2018-05-12 22:14:41 +0800

    finished dynamic updating of Accomplishments

--- js/index.js
+++ js/index.js
@@ -5,8 +5,20 @@ projects.forEach(project => project.addEventListener("click", clickOnProject, { 
 
 // texts
 const header = ["Flashcard App", "Tribute Page", "Twitch TV", "React Wikipedia", "React Quote Machine"];
-const intro = ["Created during Chingu Voyage, a collaborative environment where hundreds of people build projects together to learn. It allows users to create a topic, insert questions and answers.", "This was the second website that I created. I chose Elon Musk because I respect his tenacity, work ethics and his reasons for what he's doing.", "Tracks twitch streamers and shows what they're streaming if they're online. Allows user to filter based on status.", "Created using React, search the typed word at Wikipedia and return the top 10 results", "Created using React, generates a new design quote when it's clicked"];
-const projects0 = ["Learnt Node.js and Express", "Learnt React and Redux", "Added if user isAuthenticated", "Made it responsive", "Refactored and used table and grid"];
+const intro = [
+  "Created during Chingu Voyage, a collaborative environment where hundreds of people build projects together to learn. It allows users to create a topic, insert questions and answers.",
+  "This was the second website that I created. I chose Elon Musk because I respect his tenacity, work ethics and his reasons for what he's doing.",
+  "Tracks twitch streamers and shows what they're streaming if they're online. Allows user to filter based on status.",
+  "Created using React, search the typed word at Wikipedia and return the top 10 results",
+  "Created using React, generates a new design quote when it's clicked"
+];
+const whatILearnt = [
+  ["Learnt Node.js and Express", "Learnt React and Redux", "Added if user isAuthenticated", "Made it responsive", "Refactored and used table and grid"],
+  ["Second website I built", "Responsive image", "Refactored CSS to SCSS"],
+  ["Third website I built", "Used Twitch API", "Used jQuery, Bootstrap"],
+  ["Created with React and Babel", "Used Wikipedia OpenSearch"],
+  ["Created with React and Babel", "Used Quotes on Design API", "Used jQuery"]
+];
 
 // ul parent node
 const projectsParent = document.getElementById("projects__parent");
@@ -32,37 +44,34 @@ function clickOnProject(e) {
 
   // remove projects__flex class from clicked project, change cursor back to default
   e.path[1].classList.remove("projects__flex");
-  e.path[1].style = "cursor: default;"; //how can I style all li elements
+  e.path[1].style = "cursor: default;"; // how can I style all li elements
 
   const section = document.createElement("section");
   section.className = "container projects__description";
 
   const clickedClass = e.path[1].className;
-  console.log(clickedClass);
-  const clickedNum = clickedClass[clickedClass.length-1];
-  console.log(clickedNum);
+  const clickedNum = clickedClass[clickedClass.length - 1];
 
-  //left
-  /*
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"/></svg> */
+  const leftArrow = document.createElement("div");
+  leftArrow.className = "projects__leftArrow"
+  leftArrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" stroke-width="4"/></svg>`;
 
-  //right
-  /*
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"/></svg> */
+  const rightArrow = document.createElement("div");
+  rightArrow.className = "projects__rightArrow";
+  rightArrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" stroke-width="4"/></svg>`;
+  console.log();
+
+  let li = "";
+  for (let x = 0; x < whatILearnt[clickedNum].length; x++) {
+    li += `<li>${whatILearnt[clickedNum][x]}</li>`;
+  }
 
   // how to create multiple li element depending on how many in array
-  section.innerHTML = `<div><h3>${header[clickedNum]}</h3><p>${intro[clickedNum]}</p><hr><br><h3>Accomplishments</h3><br><li>${projects0[clickedNum]}</li></div>`;
+  section.innerHTML = `<div><h3>${header[clickedNum]}</h3><p>${intro[clickedNum]}</p><hr class="projects__hr"><h3>Accomplishments</h3>${li}</div>`;
 
   projectsParent.appendChild(section);
-}
-
-createList();
-
-function createList() {
-  let li = `<li>${projects0[0]}</li>`;
-  // error
-  console.log(li*2);
-  return li;
+  e.path[1].appendChild(leftArrow);
+  e.path[1].appendChild(rightArrow);
 }
 
 // exit project
